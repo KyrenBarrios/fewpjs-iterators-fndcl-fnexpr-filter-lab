074d0924ac640ceb519cda71acdf3830e6509c39
Kyren Barrios <kyrenbarrios@gmail.com>
2021-05-21 14:31:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+// Code your solution herel\
+function findMatching(drivers, name) {
+  return drivers.filter( d =>
+    d.toLowerCase() === name.toLowerCase()
+  )
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter( d =>
+    d.toLowerCase().indexOf(name.toLowerCase()) === 0
+  )
+}
+function matchName(drivers, name) {
+  return drivers.filter( d => d.name === name)
+}
